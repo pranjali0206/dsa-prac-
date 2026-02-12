b974d9978ea785945ab939ef7a5b77029cfa42bf
pranjali0206 <pranjalishandilya15@gmail.com>
2026-02-12 23:12:29 +0530

    Added moveZeros implementation

---
+++ D.java
@@ -0,0 +1,25 @@
+class D {
+
+    public static void moveZeros(int[] arr) {
+        int left = 0;
+
+        for (int right = 0; right < arr.length; right++) {
+            if (arr[right] != 0) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 3, 12};
+
+        moveZeros(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
