a94c5d1ccd57449d8007429d5e7897a164b10211
pranjali0206 <pranjalishandilya15@gmail.com>
2026-02-13 23:34:46 +0530

    dsa

---
+++ F.java
@@ -0,0 +1,25 @@
+class E {
+
+    static int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int max = 0;
+
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            max = Math.max(max, area);
+
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        
+        int result = maxArea(height);
+        System.out.println("Maximum Area: " + result);
+    }
+}
